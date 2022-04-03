fb29a33912d9f1e7a8dbfee890f7c04d81bf2537
WangLei <2833680178@qq.com>
2022-04-03 13:59:07 +0800

    Initial commit

---
+++ src/main/java/com/wanglei/Demo1.java
@@ -0,0 +1,8 @@
+package com.wanglei;
+
+public class Demo1 {
+    public String say(String str){
+        System.out.println("Hello "+str);
+        return "Hello "+str;
+    }
+}
---
+++ src/test/java/com/wanglei/Demo1Test.java
@@ -0,0 +1,11 @@
+package com.wanglei;
+
+import org.junit.Test;
+
+public class Demo1Test {
+    @Test
+    public void testSay(){
+        Demo1 demo1 = new Demo1();
+        demo1.say("Maven");
+    }
+}
